4a5618aec79c822239dbcf2f16495fa602005ee9
mfranck340 <franck.yaguargos@edu.uah.es>
2023-03-09 20:08:14 +0100

    Optimizacion de la regla0 y actualizar sudoku

--- Practica1/logica.pl
+++ Practica1/logica.pl
@@ -94,128 +94,69 @@ obtenerBloque(Vector, Fila, Columna, ValoresBloque) :-        %Funcion que obtie
         nth0(IndexBloque, Vector, Valor)                      %Saca el elemento del vector correspondiente al index y lo guarda en valor
     ), ValoresBloque).
 %-------------------------------------------------------------------------------
+%Sustituir un elemento
+sustituir_elemento(Vector, Indice, NuevoElemento, NuevoVector) :-
+    nth1(Indice, Vector, _, Resto),
+    nth1(Indice, NuevoVector, NuevoElemento, Resto).
+
 %Meter funcion de actualizar
 
-actualizar_sudoku(Sudoku, Resultado):-        %Funcion que actualiza el sudoku eliminando elementos repetidos en una fila, columna, bloque
-    actualizarSudokuAux(Sudoku, 81, [], Resultado).    %Llamada recursiva comenzando por posicion final
-    
-actualizarSudokuAux(_, 0, Resultado, Resultado).       %Función para terminar la recursividad
+actualizar_sudoku(Sudoku, N, Resultado):-
+     obtener_ejes(N, J1, J2),
+     nth1(N, Sudoku, X),
+     recorrerFila(Sudoku, X, 9, J1, ListaAux),
+     write(ListaAux),nl,
+     mostrar_sudoku(ListaAux),
+     %recorrerColumna(),
+     %recorrerCuadrado(),
+     Resultado = ListaAux.
 
-actualizarSudokuAux(Sudoku, N, Lista, Resultado):-
-    nth1(N, Sudoku, X),                                %Saca posición 1 del sudoku y lo guarda en X
-    valoresPosibles(Num),                              %Guarda en Num todos los valores posibles ya definidos
-    member(X, Num),
-    obtener_ejes(N, J1, J2),                           %Obtiene los ejes correpondientes al elemento N
-    actualizarFila(Sudoku, J1, X, ListaAux),           %Llama a la función de acrualizar fila pasandole el valor del eje X
-    length(ListaAux, Tamm),
-    write('Indice = '), write(Tamm),nl,
-    mostrar_sudoku(ListaAux),
-    write('Resulatado = '), write(ListaAux), nl,
-    actualizarColumna(),
-    actualizarBloque(),
-    N1 is N -1,                                         %Pasa al siguiente elemento a actualizar
-    actualizarSudokuAux(ListaAux, N1, Lista, Resultado), !.     %Llamada recursiva para actualizar el siguiente elemento
-    
-actualizarSudokuAux(Sudoku, N, Lista, Resultado):-
-    N1 is N - 1,
-    actualizarSudokuAux(Sudoku, N1, Lista, Resultado), !.
-    
-%-------------------------------------------------------------------------------
-actualizarFila(Sudoku, Fila, Elem, ListaAux):-
-    actualizarFilaAux(Sudoku, Fila, Elem, 9, [], ListaAux).
-    %write(ListaAux),nl.
-    
-actualizarFilaAux(_, _, _ , 0, ListaAux, ListaAux).
+recorrerFila(Final, _, 0, _, Final).
 
-actualizarFilaAux(Sudoku, Fila, Elem, N, Lista, ListaAux):-
-    N is Fila,
-    quitarElementoFila(Sudoku, N, 9, Elem, [], Listita),
-    N1 is N - 1,
-    actualizarFilaAux(Sudoku, Fila, Elem, N1, [Listita | Lista], ListaAux),!.
+recorrerFila(Sudoku, X, N, Fila, ListaFinal):-
+     Index is (Fila - 1) * 9 + N,
+     nth1(Index, Sudoku, Y),
+     member(X, Y),
+     select(X, Y, Borrada),
+     sustituir_elemento(Sudoku, Index, Borrada, SudokuAux),
+     N1 is N - 1,
+     recorrerFila(SudokuAux, X, N1, Fila, ListaFinal),!.
 
-actualizarFilaAux(Sudoku, Fila, Elem, N, Lista, ListaAux):-
-    %write('Elemento de entrada' ), write(Elem), nl,
-    recorrerFila(Sudoku, N, 9, [], Listita),
-    %No se puede modificar argumentos creo que es necesario crear una nueva variable
-    %write('Salida del recorrido '), write(Lista), nl,
-    N1 is N - 1,
-    actualizarFilaAux(Sudoku, Fila, Elem, N1, [Listita | Lista], ListaAux),!.
-    
-recorrerFila(_, _, 0, Final, Final).
-
-recorrerFila(Sudoku, N, M, L1, Final):-
-    %write('M '), write(M), write(' -- N'), write(N), nl,
-    Elemento is (N - 1) * 9 + M,
-    %write('Elemento '), write(Elemento), nl,
-    nth1(Elemento, Sudoku, X),
-    %write('Vamos a ver si va bien '), write(X), nl,
-    M1 is M - 1,
-    recorrerFila(Sudoku, N, M1, [X | L1], Final),!.
-    
-quitarElementoFila(_, _, 0, _, Final, Final).
-
-quitarElementoFila(Sudoku, N, M, Elem, L1, Final):-
-    %write('M '), write(M), write(' -- N'), write(N), nl,
-    Elemento is (N - 1) * 9 + M,
-    valoresPosibles(Num),
-    %write('Elemento '), write(Elemento), nl,
-    nth1(Elemento, Sudoku, X),
-    write('Si estoy AQUIIIIIIIIIIIIIIIIIIIIII'), nl,
-    member(X, Num),
-    write('Vamos a ver si va bien '), write(X), nl,
-    M1 is M - 1,
-    recorrerFila(Sudoku, N, M1, Elem, [X | L1], Final),!.
-
-quitarElementoFila(Sudoku, N, M, Elem, L1, Final):-
-    Elemento is (N - 1) * 9 + M,
-    write('No puedo continuar'), nl,
-    nth1(Elemento, Sudoku, X),
-    write('DEBUGGGGGGGGGGGGGGGG'), write(Elem),nl,
-    write(X),nl,
-    member(Elem, X),
-    select(Elem, X, Resultado),
-    write(Resultado),nl,
-    M1 is M - 1,
-    recorrerFila(Sudoku, N, M1, [Resultado | L1], Final),!.
-
-quitarElementoFila(Sudoku, N, M, Elem, L1, Final):-
-    M1 is M - 1,
-    recorrerFila(Sudoku, N, M1, [Resultado | L1], Final),!.
-
-%-------------------------------------------------------------------------------
-actualizarColumna().
-
-
-actualizarBloque().
-
+recorrerFila(Sudoku, X, N, Fila, ListaFinal):-
+     N1 is N - 1,
+     recorrerFila(Sudoku, X, N1, Fila, ListaFinal),!.
 
 %-------------------------------------------------------------------------------
 %Reglas de simplificacion
 %Regla 0 --> Si hay un lugar donde solo cabe un numero, lo escribimos en el lugar correspondiente y lo eliminamos de los lugares en los que aparezca de los que son conflictivos
 regla0(Sudoku, Resultado):-
-    regla0Aux(Sudoku, 81, [], Resultado).
+    regla0Aux(Sudoku, 81, Resultado).
 
-regla0Aux(_, 0, Resultado, Resultado).
+regla0Aux(Resultado, 0, Resultado).
 
-regla0Aux(Sudoku, N, Lista, Resultado):-
+regla0Aux(Sudoku, N, Resultado):-
     nth1(N, Sudoku, X),
     valoresPosibles(Num),
     member(X, Num),
     N1 is N - 1,
-    regla0Aux(Sudoku, N1, [X | Lista], Resultado),!.
+    regla0Aux(Sudoku, N1, Resultado),!.
 
-regla0Aux(Sudoku, N, Lista, Resultado):-
+regla0Aux(Sudoku, N, Resultado):-
     nth1(N, Sudoku, X),
     length(X, Tam),
     Tam > 1,
     N1 is N - 1,
-    regla0Aux(Sudoku, N1, [X | Lista], Resultado),!.
+    regla0Aux(Sudoku, N1, Resultado),!.
 
-regla0Aux(Sudoku, N, Lista, Resultado):-
+regla0Aux(Sudoku, N, Resultado):-
     nth1(N, Sudoku, X),
     N1 is N - 1,
     nth1(1, X, X1),
-    regla0Aux(Sudoku, N1, [X1 | Lista], Resultado),!.
+    sustituir_elemento(Sudoku, N, X1, SudokuAux),
+    write(SudokuAux), nl,
+    mostrar_sudoku(SudokuAux),
+    actualizar_sudoku(SudokuAux, N, Prueba),
+    regla0Aux(Sudoku, N1, Resultado),!.
 
 %-------------------------------------------------------------------------------
 regla1(Sudoku, Resultado):-
