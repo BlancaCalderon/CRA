4bb0ce9b8bd768cd91c870c24880a000216ebc4d
mfranck340 <franck.yaguargos@edu.uah.es>
2023-04-03 20:07:12 +0200

    sin avances

--- Practica2/sintaxis.pl
+++ Practica2/sintaxis.pl
@@ -8,9 +8,7 @@ oracion_coordinada(oc(O, CC, O)) --> oracion_simple(O), conjuncion_coordinada(CC
 
 oracion_subordinada(or(O, CS, O)) --> oracion_simple(O), conjuncion_subordinada(CS), oracion_simple(O).
 
-oracion_compuesta(ocm(OCM, OS)) --> oracion_compuesta(OCM), oracion_simple(OS).
-oracion_compuesta(ocm(OCM, OC)) --> oracion_compuesta(OCM), oracion_coordinada(OC).
-oracion_compuesta(ocm(OCM, OR)) --> oracion_compuesta(OCM), oracion_subordinada(OR).
+%Oraciones compuestas
 oracion_compuesta(ocm(OS, OS)) --> oracion_simple(OS), oracion_simple(OS).
 oracion_compuesta(ocm(OS, OC)) --> oracion_simple(OS), oracion_coordinada(OC).
 oracion_compuesta(ocm(OS, OR)) --> oracion_simple(OS), oracion_subordinada(OR).
@@ -21,46 +19,58 @@ oracion_compuesta(ocm(OR, OR)) --> oracion_subordinada(OR), oracion_subordinada(
 oracion_compuesta(ocm(OR, OS)) --> oracion_subordinada(OR), oracion_simple(OS).
 oracion_compuesta(ocm(OR, OC)) --> oracion_subordinada(OR), oracion_coordinada(OC).
 
+oracion_compuesta(ocm(OCM, OS)) --> oracion_compuesta(OCM), oracion_simple(OS).
+oracion_compuesta(ocm(OCM, OC)) --> oracion_compuesta(OCM), oracion_coordinada(OC).
+oracion_compuesta(ocm(OCM, OR)) --> oracion_compuesta(OCM), oracion_subordinada(OR).
+
+
 %-------------------------------------------------------------------------------
 %Grupos
 %-------------------------------------------------------------------------------
 %Grupo nominal
-g_nominal(gn(N)) --> nombre(N), !.
-g_nominal(gn(NP)) --> nombre_propio(NP), !.
-g_nominal(gn(Det, N)) --> determinante(Det), nombre(N), !.
-g_nominal(gn(Det, GAdj, N)) --> determinante(Det), g_adjetival(GAdj), nombre(N), !.
+g_nominal(gn(N)) --> nombre(N),!.
+g_nominal(gn(NP)) --> nombre_propio(NP).
+g_nominal(gn(Det, N)) --> determinante(Det), nombre(N).
+g_nominal(gn(Det, GAdj, N)) --> determinante(Det), g_adjetival(GAdj), nombre(N).
+g_nominal(gn(GAdj, NP)) --> g_adjetival(GAdj), nombre_propio(NP).
+g_nominal(gn(GAdv, GN)) --> g_adverbial(GAdv), g_nominal(GN).
 
-g_nominal(gn(GN, Conj, GN)) --> g_nominal(GN), conjuncion(Conj), g_nominal(GN), !.
-g_nominal(gn(GAdj, NP)) --> g_adjetival(GAdj), nombre_propio(NP), !.
-g_nominal(gn(GN, GAdv)) --> g_nominal(GN), g_adverbial(GAdv), !.
-g_nominal(gn(GAdv, GN)) --> g_adverbial(GAdv), g_nominal(GN), !.
-g_nominal(gn(GPrep, GN)) --> g_nominal(GN), g_preposicional(GPrep), !.
-
-
+g_nominal(gn(GN, Conj, GN)) --> g_nominal(GN), conjuncion_coordinada(Conj), g_nominal(GN).
+g_nominal(gn(GN, GAdv)) --> g_nominal(GN), g_adverbial(GAdv).
+g_nominal(gn(GPrep, GN)) --> g_nominal(GN), g_preposicional(GPrep).
 
 %Grupo verbal
-g_verbal(gv(GAdv, GV, GAdv)) --> g_adverbial(GAdv), g_verbal(GV), g_adverbial(GAdv).
+g_verbal(gv(GAdv, GV, GAdv)) --> g_adverbial(GAdv), g_verbal(GV), g_adverbial(GAdv),!.
+g_verbal(gv(GPrep, GV, GPrep)) --> g_preposicional(GPrep), g_verbal(GV), g_preposicional(GPrep),!.
+
+g_verbal(gv(V, GV)) --> verbo(V), g_verbal(GV),!.
+
+g_verbal(gv(V, GN)) --> verbo(V), g_nominal(GN),!.
+
+g_verbal(gv(V, GAdj)) --> verbo(V), g_adjetival(GAdj),!.
+
 g_verbal(gv(GAdv, GV)) --> g_adverbial(GAdv), g_verbal(GV).
-g_verbal(gv(GV, GAdv)) --> g_verbal(GV), g_adverbial(GAdv).
-g_verbal(gv(GPrep, GV, GPrep)) --> g_preposicional(GPrep), g_verbal(GV), g_preposicional(GPrep).
+
 g_verbal(gv(GPrep, GV)) --> g_preposicional(GPrep), g_verbal(GV).
+
+g_verbal(gv(GV, GAdv)) --> g_verbal(GV), g_adverbial(GAdv).
 g_verbal(gv(GV, GPrep)) --> g_verbal(GV), g_preposicional(GPrep).
-g_verbal(gv(V, GV)) --> verbo(V), g_verbal(GV).                                             %Recursivo
-g_verbal(gv(V, GAdj)) --> verbo(V), g_adjetival(GAdj).
-g_verbal(gv(V, GN)) --> verbo(V), g_nominal(GN).
+
 g_verbal(gv(V)) --> verbo(V).
 
+
 %Grupo adjetival
-g_adjetival(gadj(Adj, Conj, GAdj)) --> adjetivo(Adj), conjuncion(Conj), g_adjetival(GAdj).  %Recursivo
 g_adjetival(gadj(Adj)) --> adjetivo(Adj).
+g_adjetival(gadj(Adj, Conj, GAdj)) --> adjetivo(Adj), conjuncion_coordinada(Conj), g_adjetival(GAdj).
 
 %Grupo adverbial
-g_adverbial(gadv(Adv, GAdv)) --> adverbio(Adv), g_adverbial(GAdv).                          %Recursivo
 g_adverbial(gadv(Adv)) --> adverbio(Adv).
+g_adverbial(gadv(Adv, GAdv)) --> adverbio(Adv), g_adverbial(GAdv).
 
 %Grupo preposicional
-g_preposicional(gp(Prep, GN, Conj, GP)) -->  preposicion(Prep), g_nominal(GN), conjuncion(Conj), g_preposicional(GP).     %Recursivo
 g_preposicional(gp(Prep, GN)) -->  preposicion(Prep), g_nominal(GN).
+g_preposicional(gp(Prep, GN, Conj, GP)) -->  preposicion(Prep), g_nominal(GN), conjuncion_coordinada(Conj), g_preposicional(GP).
+
 
 %-------------------------------------------------------------------------------
 %Elementos
