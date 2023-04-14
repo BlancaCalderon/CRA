07759ac3d6c325b1afbd8ffdf55c227d8d20b1be
mfranck340 <franck.yaguargos@edu.uah.es>
2023-04-14 18:07:32 +0200

    Arreglo en la gramatica

--- Practica2/sintaxis.pl
+++ Practica2/sintaxis.pl
@@ -3,49 +3,34 @@
 %-------------------------------------------------------------------------------
 oracion(O) --> oracion_simple(O).
 oracion(O) --> oracion_coordinada(O).
-oracion(O) --> oracion_subordinada(O).
 oracion(O) --> oracion_compuesta(O).
 
 oracion_simple(o(GN, GV)) --> g_nominal(GN), g_verbal(GV).
 oracion_simple(o(GV, GN, Sig)) --> g_verbal(GV), g_nominal(GN), signo(Sig).
-oracion_aux(o(GV)) --> g_verbal(GV).
+oracion_simple(o(GV)) --> g_verbal(GV).
 
-oracion_coordinada(oc(O1, CC, O2)) --> oracion_simple(O1), conjuncion_coordinada(CC), oracion_simple(O2).
-oracion_coordinada(oc(O1, CC, OA)) --> oracion_simple(O1), conjuncion_coordinada(CC), oracion_aux(OA).
+oracion_coordinada(oc(O1, Conj, O2)) --> oracion_simple(O1), conjuncion(Conj), oracion_simple(O2).
 
-oracion_subordinada(or(O1, CS, O2)) --> oracion_simple(O1), conjuncion_subordinada(CS), oracion_simple(O2).
+oracion_subordinada(or(GN, GV)) --> g_nominal(GN), g_verbal(GV).
+oracion_subordinada(or(NX, GV)) --> nexo(NX), g_verbal(GV).
+oracion_subordinada(or(NX, GN, GV)) --> nexo(NX), g_nominal(GN), g_verbal(GV).
 
 %Oraciones compuestas
-oracion_compuesta(ocm(OS1, Conj, OS2)) --> oracion_simple(OS1), conjuncion(Conj), oracion_simple(OS2).
-oracion_compuesta(ocm(OS, Conj, OC)) --> oracion_simple(OS), conjuncion(Conj), oracion_coordinada(OC).
-oracion_compuesta(ocm(OS, Conj, OR)) --> oracion_simple(OS), conjuncion(Conj), oracion_subordinada(OR).
-oracion_compuesta(ocm(OC1, Conj, OC2)) --> oracion_coordinada(OC1), conjuncion(Conj), oracion_coordinada(OC2).
-oracion_compuesta(ocm(OC, Conj, OS)) --> oracion_coordinada(OC), conjuncion(Conj), oracion_simple(OS).
-oracion_compuesta(ocm(OC, Conj, OR)) --> oracion_coordinada(OC), conjuncion(Conj), oracion_subordinada(OR).
-oracion_compuesta(ocm(OR1, Conj, OR2)) --> oracion_subordinada(OR1), conjuncion(Conj), oracion_subordinada(OR2).
-oracion_compuesta(ocm(OR, Conj, OS)) --> oracion_subordinada(OR), conjuncion(Conj), oracion_simple(OS).
-oracion_compuesta(ocm(OR, Conj, OC)) --> oracion_subordinada(OR), conjuncion(Conj), oracion_coordinada(OC).
-oracion_compuesta(ocm(OS, Conj, OCM)) --> oracion_simple(OS), conjuncion(Conj), oracion_compuesta(OCM).
-oracion_compuesta(ocm(OC, Conj, OCM)) --> oracion_coordinada(OC), conjuncion(Conj), oracion_compuesta(OCM).
-oracion_compuesta(ocm(OR, Conj, OCM)) --> oracion_subordinada(OR), conjuncion(Conj), oracion_compuesta(OCM).
+%oracion_compuesta(ocm(OS1, Conj, OS2)) --> oracion_simple(OS1), conjuncion(Conj), oracion_simple(OS2).
 
-oracion_compuesta(ocm(OS1, OS2)) --> oracion_simple(OS1), oracion_simple(OS2).
+oracion_compuesta(ocm(OC, Conj, OS)) --> oracion_coordinada(OC), conjuncion(Conj), oracion_simple(OS).
+oracion_compuesta(ocm(OC1, Conj, OC2)) --> oracion_coordinada(OC1), conjuncion(Conj), oracion_coordinada(OC2).
+oracion_compuesta(ocm(OC, Conj, OCM)) --> oracion_coordinada(OC), conjuncion(Conj), oracion_compuesta(OCM).
+oracion_compuesta(ocm(OS, Conj, OC)) --> oracion_simple(OS), conjuncion(Conj), oracion_coordinada(OC).
+oracion_compuesta(ocm(OS, Conj, OCM)) --> oracion_simple(OS), conjuncion(Conj), oracion_compuesta(OCM).
+
+%oracion_compuesta(ocm(OS1, OS2)) --> oracion_simple(OS1), oracion_simple(OS2).
 oracion_compuesta(ocm(OS, OC)) --> oracion_simple(OS), oracion_coordinada(OC).
-oracion_compuesta(ocm(OS, OR)) --> oracion_simple(OS), oracion_subordinada(OR).
-oracion_compuesta(ocm(OS, OA)) --> oracion_simple(OS), oracion_aux(OA).
 oracion_compuesta(ocm(OC1, OC2)) --> oracion_coordinada(OC1), oracion_coordinada(OC2).
 oracion_compuesta(ocm(OC, OS)) --> oracion_coordinada(OC), oracion_simple(OS).
-oracion_compuesta(ocm(OC, OR)) --> oracion_coordinada(OC), oracion_subordinada(OR).
-oracion_compuesta(ocm(OC, OA)) --> oracion_coordinada(OC), oracion_aux(OA).
-oracion_compuesta(ocm(OR1, OR2)) --> oracion_subordinada(OR1), oracion_subordinada(OR2).
-oracion_compuesta(ocm(OR, OS)) --> oracion_subordinada(OR), oracion_simple(OS).
-oracion_compuesta(ocm(OR, OC)) --> oracion_subordinada(OR), oracion_coordinada(OC).
-oracion_compuesta(ocm(OR, OA)) --> oracion_subordinada(OR), oracion_aux(OA).
 
 oracion_compuesta(ocm(OS, OCM)) --> oracion_simple(OS), oracion_compuesta(OCM).
 oracion_compuesta(ocm(OC, OCM)) --> oracion_coordinada(OC), oracion_compuesta(OCM).
-oracion_compuesta(ocm(OR, OCM)) --> oracion_subordinada(OR), oracion_compuesta(OCM).
-oracion_compuesta(ocm(OR, OA)) --> oracion_subordinada(OR), oracion_aux(OA).
 
 %-------------------------------------------------------------------------------
 %Grupos
@@ -54,40 +39,42 @@ oracion_compuesta(ocm(OR, OA)) --> oracion_subordinada(OR), oracion_aux(OA).
 g_nominal(gn(N)) --> nombre(N).
 g_nominal(gn(N, GN)) --> nombre(N), g_nominal(GN).
 g_nominal(gn(NP)) --> nombre_propio(NP).
+g_nominal(gn(NP, OR)) --> nombre_propio(NP), oracion_subordinada(OR).
 g_nominal(gn(NP, GN)) --> nombre_propio(NP), g_nominal(GN).
 g_nominal(gn(GAdv, N)) --> g_adverbial(GAdv), nombre(N).
 g_nominal(gn(GAdv, NP)) --> g_adverbial(GAdv), nombre_propio(NP).
 g_nominal(gn(Det, N)) --> determinante(Det), nombre(N).
+g_nominal(gn(Det, N, OR)) --> determinante(Det), nombre(N), oracion_subordinada(OR).
 g_nominal(gn(Det, N, GN)) --> determinante(Det), nombre(N), g_nominal(GN).
 g_nominal(gn(Det, GAdj, N)) --> determinante(Det), g_adjetival(GAdj), nombre(N).
 g_nominal(gn(Det, GAdv, N)) --> determinante(Det), g_adverbial(GAdv), nombre(N).
-g_nominal(gn(Det, GAdv, GAdj, N)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), nombre(N).
-g_nominal(gn(Det, GAdj, GN)) --> determinante(Det), g_adjetival(GAdj), g_nominal(GN).
+%g_nominal(gn(Det, GAdv, GAdj, N)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), nombre(N).
+%g_nominal(gn(Det, GAdj, GN)) --> determinante(Det), g_adjetival(GAdj), g_nominal(GN).
 g_nominal(gn(Det, GAdv, GN)) --> determinante(Det), g_adverbial(GAdv), g_nominal(GN).
-g_nominal(gn(Det, GAdv, GAdj, GN)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), g_nominal(GN).
+%g_nominal(gn(Det, GAdv, GAdj, GN)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), g_nominal(GN).
 g_nominal(gn(GAdj, N)) --> g_adjetival(GAdj), nombre(N).
 g_nominal(gn(GAdj, NP)) --> g_adjetival(GAdj), nombre_propio(NP).
 g_nominal(gn(GAdv, GAdj, N)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre(N).
 g_nominal(gn(GAdv, GAdj, NP)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre_propio(NP).
 g_nominal(gn(GPrep, GN)) --> g_preposicional(GPrep), g_nominal(GN).
 
-g_nominal(gn(N, Conj, GN)) --> nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(NP, Conj, GN)) --> nombre_propio(NP), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdv, N, Conj, GN)) --> g_adverbial(GAdv), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdv, NP, Conj, GN)) --> g_adverbial(GAdv), nombre_propio(NP), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(Det, N, Conj, GN)) --> determinante(Det), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(Det, GAdj, N, Conj, GN)) --> determinante(Det), g_adjetival(GAdj), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(Det, GAdv, N, Conj, GN)) --> determinante(Det), g_adverbial(GAdv), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(Det, GAdv, GAdj, N, Conj, GN)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdj, N, Conj, GN)) --> g_adjetival(GAdj), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdj, NP, Conj, GN)) --> g_adjetival(GAdj), nombre_propio(NP), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdv, GAdj, N, Conj, GN)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre(N), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GAdv, GAdj, NP, Conj, GN)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre_propio(NP), conjuncion_coordinada(Conj), g_nominal(GN).
-g_nominal(gn(GPrep, GN1, Conj, GN2)) --> g_preposicional(GPrep), g_nominal(GN1), conjuncion_coordinada(Conj), g_nominal(GN2).
+g_nominal(gn(N, Conj, GN)) --> nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(NP, Conj, GN)) --> nombre_propio(NP), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdv, N, Conj, GN)) --> g_adverbial(GAdv), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdv, NP, Conj, GN)) --> g_adverbial(GAdv), nombre_propio(NP), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(Det, N, Conj, GN)) --> determinante(Det), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(Det, GAdj, N, Conj, GN)) --> determinante(Det), g_adjetival(GAdj), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(Det, GAdv, N, Conj, GN)) --> determinante(Det), g_adverbial(GAdv), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(Det, GAdv, GAdj, N, Conj, GN)) --> determinante(Det), g_adverbial(GAdv), g_adjetival(GAdj), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdj, N, Conj, GN)) --> g_adjetival(GAdj), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdj, NP, Conj, GN)) --> g_adjetival(GAdj), nombre_propio(NP), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdv, GAdj, N, Conj, GN)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre(N), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GAdv, GAdj, NP, Conj, GN)) --> g_adverbial(GAdv), g_adjetival(GAdj), nombre_propio(NP), conjuncion(Conj), g_nominal(GN).
+g_nominal(gn(GPrep, GN1, Conj, GN2)) --> g_preposicional(GPrep), g_nominal(GN1), conjuncion(Conj), g_nominal(GN2).
 
 %Grupo verbal
 g_verbal(gv(V)) --> verbo(V).
-g_verbal(gv(V, GV)) --> verbo(V), g_verbal(GV).
+%g_verbal(gv(V, GV)) --> verbo(V), g_verbal(GV).
 g_verbal(gv(V, GN)) --> verbo(V), g_nominal(GN).
 g_verbal(gv(V, GAdj)) --> verbo(V), g_adjetival(GAdj).
 g_verbal(gv(V, GAdv)) --> verbo(V), g_adverbial(GAdv).
@@ -103,7 +90,7 @@ g_verbal(gv(GPrep1, GV, GPrep2)) --> g_preposicional(GPrep1), g_verbal(GV), g_pr
 
 %Grupo adjetival
 g_adjetival(gadj(Adj)) --> adjetivo(Adj).
-g_adjetival(gadj(Adj, Conj, GAdj)) --> adjetivo(Adj), conjuncion_coordinada(Conj), g_adjetival(GAdj).
+g_adjetival(gadj(Adj, Conj, GAdj)) --> adjetivo(Adj), conjuncion(Conj), g_adjetival(GAdj).
 
 %Grupo adverbial
 g_adverbial(gadv(Adv)) --> adverbio(Adv).
@@ -123,11 +110,9 @@ verbo(v(X)) --> [X], {v(X)}.
 adjetivo(adj(X)) --> [X], {adj(X)}.
 adverbio(adv(X)) --> [X], {adv(X)}.
 conjuncion(conj(X)) --> [X], {conj(X)}.
-conjuncion_coordinada(conjc(X)) --> [X], {conjc(X)}.
-conjuncion_subordinada(conjs(X)) --> [X], {conjs(X)}.
 preposicion(prep(X)) --> [X], {prep(X)}.
 signo(sig(X)) --> [X], {sig(X)}.
-pronoun(pron(X)) --> [X], {pron(X)}.
+nexo(nx(X)) --> [X], {nx(X)}.
 
 
 %-------------------------------------------------------------------------------
@@ -207,8 +192,6 @@ n('cat').
 n('neighbor').
 n('evenings').
 n('tool').
-n('who').
-n('which').
 n('it').
 n('yesterday').
 n('we').
@@ -293,7 +276,6 @@ conj('but').
 conj('or').
 conj('so').
 conj('yet').
-conj('for').
 conj('nor').
 conj('after').
 conj('although').
@@ -314,41 +296,6 @@ conj('where').
 conj('wherever').
 conj('while').
 conj('that').
-
-%-------------------------------------------------------------------------------
-%Conjunciones Coordinadas
-%-------------------------------------------------------------------------------
-conjc('and').
-conjc(',').
-conjc('but').
-conjc('or').
-conjc('so').
-conjc('yet').
-conjc('for').
-conjc('nor').
-conjc('that').
-
-%-------------------------------------------------------------------------------
-%Conjunciones Subordinadas
-%-------------------------------------------------------------------------------
-conjs('after').
-conjs('although').
-conjs('as').
-conjs('as if').
-conjs('because').
-conjs('before').
-conjs('even though').
-conjs('if').
-conjs('in order that').
-conjs('once').
-conjs('provided that').
-conjs('rather than').
-conjs('than').
-conjs('though').
-conjs('when').
-conjs('where').
-conjs('wherever').
-conjs('while').
 
 %-------------------------------------------------------------------------------
 %Preposiciones
@@ -390,6 +337,13 @@ prep('without').
 %-------------------------------------------------------------------------------
 %Signos
 %-------------------------------------------------------------------------------
+nx('that').
+nx('which').
+nx('who').
+
+%-------------------------------------------------------------------------------
+%Signos
+%-------------------------------------------------------------------------------
 sig('?').
 sig('!').
 
@@ -404,9 +358,9 @@ oracion5(['Jose', 'and', 'Hector', 'eat', 'french fries', 'and', 'drink', 'beer'
 oracion6(['Jose', 'eats', 'french fries', 'but', 'Maria', 'prefers', 'paella', 'although', 'Hector', 'drinks', 'coffee', 'and', 'Irene', 'reads', 'a', 'novel']).
 oracion7(['Irene', 'sings', 'and', 'jumps', 'while', 'Jose', 'studies']).
 oracion8(['Hector', 'eats', 'french fries', 'and', 'drinks', 'juice', 'while', 'Jose', 'sings', 'and', 'jumps', 'although', 'Maria', 'reads', 'a', 'novel']).
-oracion9(['Jose', 'who', 'is', 'agile', ',', 'climbs', 'at', 'the', 'climbing', 'wall', 'in', 'the', 'evenings']).
-oracion10(['Jose', 'who', 'is', 'very', 'picky', ',', 'only', 'eats', 'red', 'apples']).
-oracion11(['The', 'word processor', 'which', 'is', 'a', 'quite', 'powerful', 'tool', ',', 'is used', 'for', 'writing', 'documents']).
+oracion9(['Jose', 'who', 'is', 'agile', 'climbs', 'at', 'the', 'climbing', 'wall', 'in', 'the', 'evenings']).
+oracion10(['Jose', 'who', 'is', 'very', 'picky', 'only', 'eats', 'red', 'apples']).
+oracion11(['The', 'word processor', 'which', 'is', 'a', 'quite', 'powerful', 'tool', 'is used', 'for', 'writing', 'documents']).
 oracion12(['The', 'word processor', 'is', 'a', 'very', 'powerful', 'tool', 'that', 'is used', 'for', 'writing', 'documents', 'but', 'it', 'is', 'quite', 'slow']).
 oracion13(['The', 'mouse', 'that', 'the', 'cat', 'caught', 'was', 'gray']).
 oracion14(['The', 'man', 'we', 'saw', 'yesterday', 'was', 'my', 'neighbor']).
@@ -425,9 +379,13 @@ obtener_oracion(_, 0, Fin, Fin).
 obtener_oracion(Oracion, 1, Lista, Final):-
     X = Oracion,
     (
-    o(_, _) = X,
-    Y = [X | Lista],
-    obtener_oracion(Oracion, 0, Y, Final),!
+    o(Sujeto, _) = X,
+    es_subordinada(Sujeto, Continuar),
+    (Continuar is 1 ->
+    (write('Funciona'))
+    ;
+    (Y = [X | Lista],
+    obtener_oracion(Oracion, 0, Y, Final),!))
     ;
     o(Predicado) = X,
     nth1(1, Lista, Elem),
@@ -446,13 +404,6 @@ obtener_oracion(Oracion, 1, Lista, Final):-
 
 obtener_oracion(Oracion, 1, Lista, Final):-
     X = Oracion,
-    or(Simple, _, Simple2) = X,
-    obtener_oracion(Simple, 1, Lista, Lista1),!,
-    obtener_oracion(Simple2, 1, Lista1, Lista2),!,
-    obtener_oracion(Oracion, 0, Lista2, Final),!.
-
-obtener_oracion(Oracion, 1, Lista, Final):-
-    X = Oracion,
     (
     ocm(Simple, Simple2) = X
     ;
@@ -461,6 +412,20 @@ obtener_oracion(Oracion, 1, Lista, Final):-
     obtener_oracion(Simple, 1, Lista, Lista1),!,
     obtener_oracion(Simple2, 1, Lista1, Lista2),!,
     obtener_oracion(Oracion, 0, Lista2, Final),!.
+    
+es_subordinada(Sujeto, Salida):-
+    term_to_atom(Sujeto, Atom),
+    atomic_list_concat(Atoms, 'or', Atom),
+    maplist(atom_string, Atoms, Lista),
+    length(Lista, Tam),
+    write(Lista), nl,
+    (Tam is 2,
+    Salida = 1;
+    Salida = 0).
+    
+%obtener_subordinadas(_, Final, Final).
+
+%obtener_subordinadas(Oracion, Lista, Final):-
 
 %Metodos para limpiar oraciones
 %-------------------------------------------------------------------------------
@@ -469,7 +434,6 @@ limpiar_oraciones([ ], Final, Final).
 limpiar_oraciones([Head | Tail], Final, Aux):-
     limpiar_oracion(Head, Resultado),
     limpiar_oraciones(Tail, [Resultado | Final], Aux).
-
 
 limpiar_oracion(Oracion, Cadena):-
     term_to_atom(Oracion, Atom),
@@ -489,7 +453,6 @@ procesar_elementos([Head|Tail], Final, Aux):-
     eliminar_caracter(Resultado, '\'', Resultado2),
     append(Final, [Resultado2], ListaFinal),
     procesar_elementos(Tail, ListaFinal, Aux).
-
 
 eliminar_caracter(Cadena, Caracter, NuevaCadena) :-
     atom_chars(Cadena, ListaCaracteres),
